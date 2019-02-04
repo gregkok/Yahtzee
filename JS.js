b49ad63b5145b69040578e552e124e65bf237440
Greg Kokkoris <grkokkoris@gmail.com>
2019-02-04 15:28:01 +0100

    new table

---
+++ JS.js
@@ -0,0 +1,9 @@
+var players={player_1, player_2, player_3, player_4};
+
+players[0] = {ones, twos, threes, fours, fives, sixes, half_time, half_bonus, up_total, three_kind, four_kind, full_house, small, large, yahtzee, chance, yah_bonus, low_total, grand_total}
+players[1] = {ones, twos, threes, fours, fives, sixes, half_time, half_bonus, up_total, three_kind, four_kind, full_house, small, large, yahtzee, chance, yah_bonus, low_total, grand_total}
+players[2] = {ones, twos, threes, fours, fives, sixes, half_time, half_bonus, up_total, three_kind, four_kind, full_house, small, large, yahtzee, chance, yah_bonus, low_total, grand_total}
+players[3] = {ones, twos, threes, fours, fives, sixes, half_time, half_bonus, up_total, three_kind, four_kind, full_house, small, large, yahtzee, chance, yah_bonus, low_total, grand_total}
+
+players[0].ones = input_of_line_1
+if (row =)
